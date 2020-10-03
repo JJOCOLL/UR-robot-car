b3f4127df87a18000c89a0c6fbad7054a060d44d
Justin-LIANG <45960024+JJOCOLL@users.noreply.github.com>
2020-10-03 22:36:59 +0800

    Update HC-SR04_sensor_data_filter.ino

--- HC-SR04_sensor_data_filter.ino
+++ HC-SR04_sensor_data_filter.ino
@@ -1,22 +1,23 @@
 long getDistance(long distance, int direct){
   int back = 0;     
-  while(distance > 200 && distance < 0){
+  while(distance > 200 || distance < 0){
     if(back > 10){        //go back when always checking distance
-      moveBackward(50);
+      moveBackward(255);
+      moveBackward(255);
     }
     
     switch(direct){
-    case 1:   //check left distance
+    case 1:   //check left distance again
       back++;
-      distance = sr04_left.Distance();//读取超声波距离
+      distance = sr04_left.Distance();
       break;
-    case 2:   //check front distance
+    case 2:   //check front distance again
       back++;
-      distance = sr04_front.Distance();//读取超声波距离
+      distance = sr04_front.Distance();
       break;
-    case 3:   //check right distance
+    case 3:   //check right distance again
       back++;
-      distance = sr04_right.Distance();//读取超声波距离
+      distance = sr04_right.Distance();
       break;
     }
   }
@@ -27,13 +28,13 @@ long getDistance(long distance, int direct){
 void check_distance(int direct){    
   switch(direct){
     case 1:   //get left distance
-      Left_Distance = getDistance(sr04_left.Distance(), 1);//读取超声波距离
+      Left_Distance = getDistance(sr04_left.Distance(), 1);
       break;
     case 2:   //get front distance
-      Front_Distance = getDistance(sr04_front.Distance(), 2);//读取超声波距离
+      Front_Distance = getDistance(sr04_front.Distance(), 2);
       break;
     case 3:   //get right distance
-      Right_Distance = getDistance(sr04_right.Distance(), 3);//读取超声波距离
+      Right_Distance = getDistance(sr04_right.Distance(), 3);
       break;
   }
 }
